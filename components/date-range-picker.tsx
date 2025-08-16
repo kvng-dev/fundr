59cc6e2ee3dbbe7bd3b8514f50cea7f68ac33827
kvng-dev <oghuanlan@gmail.com>
2025-08-16 13:17:58 +0100

    fix jsx

--- components/date-range-picker.tsx
+++ components/date-range-picker.tsx
@@ -99,7 +99,7 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
   align = "end",
   locale = "en-US",
   showCompare = true,
-}): JSX.Element => {
+}): React.ReactElement => {
   const [isOpen, setIsOpen] = useState(false);
 
   const [range, setRange] = useState<DateRange>({
@@ -120,8 +120,8 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
   );
 
   // Refs to store the values of range and rangeCompare when the date picker is opened
-  const openedRangeRef = useRef<DateRange | undefined>();
-  const openedRangeCompareRef = useRef<DateRange | undefined>();
+  const openedRangeRef = useRef<DateRange | undefined>(undefined);
+  const openedRangeCompareRef = useRef<DateRange | undefined>(undefined);
 
   const [selectedPreset, setSelectedPreset] = useState<string | undefined>(
     undefined
@@ -300,7 +300,7 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
     preset: string;
     label: string;
     isSelected: boolean;
-  }): JSX.Element => (
+  }): React.ReactElement => (
     <Button
       className={cn(isSelected && "pointer-events-none")}
       variant="ghost"
